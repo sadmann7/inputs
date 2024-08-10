7d15a8f652df7c1ab3f272accd4b1ee7622e740d
sadmann7 <sadmann898@gmail.com>
2024-08-10 21:16:11 +0600

    chore: update config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,13 @@
-import { env } from "@/env"
+import { env } from "@/env";
 
-export type SiteConfig = typeof siteConfig
+export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Inputs",
-  description: "Collection of input fields built with shadcn-ui",
+  description: "Collection of input fields built with shadcn-ui and radix-ui.",
   url:
     env.NODE_ENV === "development"
       ? "http://localhost:3000"
       : "https://inputs.sadmn.com",
   links: { github: "https://github.com/sadmann7/inputs" },
-}
+};
