5b9f707260c64b3878bfdecb2bea27b6c380f13b
sadmann7 <sadmann898@gmail.com>
2024-08-10 21:15:03 +0600

    feat: add ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Shell } from "@/components/shell"
+
+export default function IndexPage() {
+  return (
+    <Shell>
+      <h1>Inputs</h1>
+    </Shell>
+  )
+}
---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+import { env } from "@/env"
+
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Inputs",
+  description: "Collection of input fields built with shadcn-ui",
+  url:
+    env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://inputs.sadmn.com",
+  links: { github: "https://github.com/sadmann7/inputs" },
+}
